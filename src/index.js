a9e5e78674e27b0e9537c5f378788a0e15df435a
D SURYA <dhs32747138@gmail.com>
2023-11-13 22:40:12 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { PORT } = require("./config/Serverconfig");
+var bodyParser = require("body-parser");
+
 const startFlightService = async () => {
   const app = express();
-
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
+  // parse application/json
+  app.use(bodyParser.json());
   app.listen(PORT, () => {
     console.log(`Started Flight Services at port ${PORT}`);
   });
